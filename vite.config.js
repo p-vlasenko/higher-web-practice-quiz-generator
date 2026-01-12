56475ffe18ab8b8f288526d5554db66903cf92b0
Pavel Vlasenko <p.s.vlas@yandex.ru>
2026-01-12 14:20:49 +0300

    add vit config

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,31 @@
-// Напишите конфиг
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+
+const src = resolve(__dirname, 'src');
+
+export default defineConfig({
+	base: './',
+	resolve: {
+		alias: {
+			'@': src,
+		}
+	},
+	server: {
+		port: 3000,
+		open: true
+	},
+	build: {
+		outDir: resolve(__dirname, 'dist'),
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				quizzes: resolve(__dirname, 'quizzes.html'),
+				quiz: resolve(__dirname, 'quiz.html')
+			}
+		}
+	},
+	optimizeDeps: {
+		include: ['zod', 'idb', 'uuid']
+	}
+});
