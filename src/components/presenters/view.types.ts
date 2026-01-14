7ef1c5ef0f677df64ad45970c6f87dcd8738bf5c
Pavel Vlasenko <pavel.vlasenko@kaspersky.com>
2026-01-14 23:28:42 +0300

    add view types

--- src/components/presenters/view.types.ts
+++ src/components/presenters/view.types.ts
@@ -1,11 +1,50 @@
-import type { QuizAddingParams } from '@/messaging/commands';
+import type { QuizAddingParams, SelectedOptions } from '@/messaging/commands';
 import type { Observable } from '@/utils/observable';
+import type { ResultInfo } from './types';
+import type { JsonParsingError } from '@/errors/json-parsing.error';
+import type { ValidationError } from '@/errors/validation.error';
+import type { Quiz, QuizId } from '@/types/quiz';
+import type { Renderable, Renderer } from '@/types/base';
+import type { QuestionData } from '@/types/view';
+
+export type QuizCardViewEvents = {
+    ['start_quiz_click']: { quizId: QuizId };
+};
+
+export type QuizCardView = Observable<QuizCardViewEvents>
+  & Renderable
+  & Renderer<Quiz>;
+
+export type QuizzesView = Observable<QuizCardViewEvents>
+  & Renderer<QuizCardView[]>
+  & { clear(): void };
 
 export type QuizGeneratorViewEvents = {
     ['submit']: QuizAddingParams;
+    ['validation_error']: JsonParsingError | ValidationError;
 };
 
 export type QuizGeneratorView = Observable<QuizGeneratorViewEvents> & {
-    toValid(): void;
-    toInvalid(): void;
+    clear(): void;
+};
+
+export type QuizGameResultViewEvents = {
+    ['restart']: undefined;
+};
+
+export type QuizGameResultView = Renderer<ResultInfo> & Observable<QuizGameResultViewEvents>;
+
+export type QuestionViewEvents = {
+    ['submit']: SelectedOptions;
+    ['next_button_click']: undefined;
+};
+
+export type RenderWithAnswerParams = QuestionData & { forwardButtonText: string };
+
+export type QuestionView = Renderer<QuestionData> & Observable<QuestionViewEvents> & {
+    renderWithAnswer(params: RenderWithAnswerParams): void;
+};
+
+export type QuizCardFactory = {
+    make(quiz: Quiz): QuizCardView;
 };
